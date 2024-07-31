501b0dde9dede274bcdf04b94a055ba998715d32
Alejandro Sosa <alesjohnson@hotmail.com>
2024-07-31 17:32:31 +0200

    Added some comments

--- internal/app/core/rover.go
+++ internal/app/core/rover.go
@@ -19,6 +19,8 @@ type Rover struct {
 	Direction Direction
 }
 
+// NewRover creates a new rover with the specified position and direction.
+// It returns a pointer to the Rover instance.
 func NewRover(x, y int, direction Direction) *Rover {
 	return &Rover{
 		Position:  Position{X: x, Y: y},
