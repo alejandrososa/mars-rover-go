d2f8c6591cb9d71a2edff0b8221ed6b3d84a0c67
Alejandro Sosa <alesjohnson@hotmail.com>
2024-07-31 19:39:05 +0200

    Platform was refactored

--- internal/app/platform/platform.go
+++ internal/app/platform/platform.go
@@ -5,12 +5,27 @@ import "github.com/alejandrososa/mars-rover-go/internal/app/ports"
 // Platform represents the surface on which the rover is moving.
 type Platform struct {
 	Width, Height int
-	obstacles     []ports.Position
+	Obstacles     []ports.Position
+	// AllowWrapAround determines whether the rover wraps around the platform's boundaries.
+	// If true, the platform has no physical limits, and the rover reappears on the opposite side when crossing an edge.
+	// Example: From (9, 5) moving east results in (0, 5).
+	// If false, the platform has physical limits, and the rover stops at the edge without wrapping around.
+	// Example: From (9, 5) moving east keeps the rover at (9, 5).
+	AllowWrapAround bool
 }
 
 // NewPlatform creates a new instance of Platform with the given dimensions and obstacles.
-func NewPlatform(width, height int, obstacles []ports.Position) *Platform {
-	return &Platform{Width: width, Height: height, obstacles: obstacles}
+func NewPlatform(width, height int, obstacles []ports.Position, options ...bool) *Platform {
+	allowWrapAround := false // value by default
+	if len(options) > 0 {
+		allowWrapAround = options[0]
+	}
+	return &Platform{
+		Width:           width,
+		Height:          height,
+		Obstacles:       obstacles,
+		AllowWrapAround: allowWrapAround,
+	}
 }
 
 // IsValidPosition checks if a position (x, y) is valid on the platform
@@ -18,7 +33,7 @@ func (p *Platform) IsValidPosition(x, y int) bool {
 	if x < 0 || x >= p.Width || y < 0 || y >= p.Height {
 		return false
 	}
-	for _, obstacle := range p.obstacles {
+	for _, obstacle := range p.Obstacles {
 		if obstacle.X == x && obstacle.Y == y {
 			return false
 		}
@@ -28,5 +43,5 @@ func (p *Platform) IsValidPosition(x, y int) bool {
 
 // SetObstacles updates the obstacles on the platform
 func (p *Platform) SetObstacles(obstacles []ports.Position) {
-	p.obstacles = obstacles
+	p.Obstacles = obstacles
 }
--- internal/app/platform/platform_test.go
+++ internal/app/platform/platform_test.go
@@ -3,6 +3,7 @@ package platform_test
 import (
 	"testing"
 
+	"github.com/alejandrososa/mars-rover-go/internal/app/adapters/mock"
 	"github.com/alejandrososa/mars-rover-go/internal/app/platform"
 	"github.com/alejandrososa/mars-rover-go/internal/app/ports"
 )
@@ -74,5 +75,43 @@ func TestRemoveObstacles(t *testing.T) {
 
 	if !plt.IsValidPosition(2, 2) {
 		t.Errorf("Expected position (2, 2) to be valid after removing obstacles")
+	}
+}
+
+func TestAllowWrapAroundTrue(t *testing.T) {
+	plt := platform.NewPlatform(10, 10, []ports.Position{}, true)
+
+	// Test wrap-around on the Y-axis
+	rover, _ := mock.NewMockRover(5, 9, "N", plt)
+	rover.Move()
+	x, y := rover.GetPosition()
+
+	if x != 5 || y != 0 {
+		t.Errorf("Expected position to wrap-around to (5, 0), got (%d, %d)", x, y)
+	}
+
+	// Test wrap-around on the X-axis
+	rover, _ = mock.NewMockRover(9, 5, "E", plt)
+	rover.Move()
+	x, y = rover.GetPosition()
+
+	if x != 0 || y != 5 {
+		t.Errorf("Expected position to wrap-around to (0, 5), got (%d, %d)", x, y)
+	}
+}
+
+func TestAllowWrapAroundFalse(t *testing.T) {
+	plt := platform.NewPlatform(10, 10, []ports.Position{}, false)
+
+	// Test no wrap-around on the X-axis
+	x, y := plt.Width-1, 5
+	if plt.IsValidPosition(x+1, y) {
+		t.Errorf("Expected position to not be valid beyond the boundary (10, 5)")
+	}
+
+	// Test no wrap-around on the Y-axis
+	x, y = 5, plt.Height-1
+	if plt.IsValidPosition(x, y+1) {
+		t.Errorf("Expected position to not be valid beyond the boundary (5, 10)")
 	}
 }
