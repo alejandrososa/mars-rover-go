782f987a13bca5a1fa53647ab7cc92600f158ceb
Alejandro Sosa <alesjohnson@hotmail.com>
2024-07-31 18:14:15 +0200

    Changed properties visibility

--- internal/app/platform/platform.go
+++ internal/app/platform/platform.go
@@ -4,18 +4,18 @@ import "github.com/alejandrososa/mars-rover-go/internal/app/ports"
 
 // Platform represents the surface on which the rover is moving.
 type Platform struct {
-	width, height int
+	Width, Height int
 	obstacles     []ports.Position
 }
 
 // NewPlatform creates a new instance of Platform with the given dimensions and obstacles.
 func NewPlatform(width, height int, obstacles []ports.Position) *Platform {
-	return &Platform{width: width, height: height, obstacles: obstacles}
+	return &Platform{Width: width, Height: height, obstacles: obstacles}
 }
 
 // IsValidPosition checks if a position (x, y) is valid on the platform
 func (p *Platform) IsValidPosition(x, y int) bool {
-	if x < 0 || x >= p.width || y < 0 || y >= p.height {
+	if x < 0 || x >= p.Width || y < 0 || y >= p.Height {
 		return false
 	}
 	for _, obstacle := range p.obstacles {
